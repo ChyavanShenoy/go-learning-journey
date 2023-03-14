29e6178df15e25e7a12c09301df5215600c81006
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2023-03-14 23:40:50 +0530

    todo initial

---
+++ todo-cli/todo.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hi there! I'm a todo app. I'm not very useful yet.")
+	fmt.Println("I'm going to be a todo app that can add, remove, and list todos.")
+	fmt.Println("I will be taking care of all your todo needs.")
+}
